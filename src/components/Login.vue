d6b6e89b7eac0c32922d00d7824c5bb003dde2bb
jorinho <jorinho@jota.one>
2025-04-23 10:48:08 +0200

    Login button as vue island

---
+++ src/components/Login.vue
@@ -0,0 +1,9 @@
+<template>
+<button class="btn btn-primary px-5 py-2 font-semibold rounded-lg cursor-pointer" @click="openLoginModal">Login</button>
+</template>
+
+<script setup lang="ts">
+const openLoginModal = () => {
+    console.log('Login button clicked');
+}
+</script>
